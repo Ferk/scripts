968bda649af1c8e0528ad84245dec6ec10171872
Ferk <ferkiwi@gmail.com>
2012-01-03 21:33:25 +0100

    Using WebM now.

--- ffscreencast.sh
+++ ffscreencast.sh
@@ -1,16 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
 ## Script to record a screencast
 
-START="-ss 00:05:00"
-END="-t  00:10:00"
+#START="-ss 00:05:00"
+END="-t  0:30:00"
 
-RATE="-r 25"
-SCREEN="-s 1280x800"
+FNAME="screencast-$(date +%m%d-%H%M).webm"
+
+SCRSIZE=$(xrandr | grep "*")
+SCRSIZE=$(echo $SCRSIZE | cut -f 1 -d " ")
+
+RATE="-r 15"
+SCREEN="-s $SCRSIZE"
 #xrandr | grep "*" | cut  -f 4 -d " "
 
-#CODEC="-acodec pcm_s16le -vcodec libx264 -vpre lossless_ultrafast"
-CODEC="-acodec libvorbis -ab 128k -ac 2"
+#CODEC="-acodec pcm_s16le -vcodec libx264"
+CODEC="-acodec libvorbis -b:a 64k -b:v 256k"
 
+#CODEC="$CODEC -vcodec libx264"
 
-ffmpeg $START $END -f alsa -i pulse -f x11grab $RATE $SCREEN -i :0.0 $CODEC output.mkv
+# Record from microphone
+A_IN="-f pulse -i default"
+# Record from system output
+A_IN="-f pulse -i 0"
+
+#ffmpeg -f alsa -i pulse -f x11grab -s 1024x600 -r 24 -b 100k -bf 2 -g 300 -i :0.0 -ar 22050 -ab 64k -acodec libmp3lame output.mpg
+
+ffmpeg $START $END $A_IN -loglevel info -f x11grab $RATE $SCREEN -i :0.0 $CODEC $@ "$FNAME"
+
+echo -e "** Video saved as \e[33m\"$FNAME\"\e[0m"
