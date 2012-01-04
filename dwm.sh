d7eb394238862a3487c7416e4e782fcda21ac899
Ferk <ferkiwi@gmail.com>
2012-01-04 16:35:03 +0100

    Added dwm-sprinkles status line color/icons in formatting.

--- dwm.sh
+++ dwm.sh
@@ -3,24 +3,26 @@
 # Fernando Carmona Varo
 
 ##
-# Wrapper script around my dwm window manager instalation
+# Wrapper script around my dwm
 ##
 
 # Loop to update the status info in the bar
 while true
 do
     TIME=$(date '+%a %F --%H:%M--')
-    LOAD=$(uptime | sed 's/.*,//')
+    LOAD=$(uptime | sed 's/.*, //')
 
     #BAT=$(acpi -b | cut -d, -f 2)
     STAT=$(statck -1)
+    TRAFIC=$(vnstat --oneline | cut -d ";" -f 11)
 
-    xsetroot -name "$TIME {$STAT} <$LOAD>"
+    #xsetroot -name "$TIME {$STAT} <$LOAD> -$TRAFIC-"
+    xsetroot -name "^[f3;$TIME ^[f2;^[i2;^[f0;$STAT^[i29; ^[f77F;$LOAD^[i15; ^[f292;^[i35;$TRAFIC "
     sleep 30
 done &
 
 #trayer --expand true --widthtype request --transparent true --alpha 255 --edge top --align right --height 15 &
-trayer &
+#trayer &
 
 # Set a known name to the WM so that some programs don't complain
 wmname LG3D
@@ -29,5 +31,4 @@ wmname LG3D
 while [ 1 ]
 do
     ~/bin/dwm/dwm
-    #wmii
 done
