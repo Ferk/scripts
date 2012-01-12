38d236ce66cc169cd1790e53f82a1b5ee59ae4b9
Ferk <ferkiwi@gmail.com>
2012-01-12 12:35:10 +0100

    Added checklight.sh script

---
+++ checklight.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+#
+# Captures from the camera and changes the brightness of the
+# screen backlight according to the brightness level of the
+# image captured by the cam.
+#
+# Uses ffmpeg, imagemagick and acpi
+#
+
+# Time for the camera to capture
+TIMECK=0.05
+
+# Minimal percent for a change to be made
+TOLERANCE=5
+
+# Limits in brightness level detected
+UPPER=40
+LOWER=25
+
+acpi -a | grep "off-line" && { # change limits if on battery
+    UPPER=60
+    LOWER=30
+}
+
+# This makes it easier for the script to be run by cron
+[ -z $DISPLAY ] && export DISPLAY=:0
+
+IMG=$(mktemp --suffix=.jpg)
+ffmpeg -loglevel error -f video4linux2  -i /dev/video0 -r  $TIMECK  -t  $TIMECK  "$IMG" || exit $?
+BRIGHT=$(convert "$IMG"  -format "%[fx:100*mean]" info:)
+
+echo "Brightness detected: ${BRIGHT}%"
+
+[ ${BRIGHT%.*} -lt $LOWER ] && {
+    LIGHT=0
+} || { 
+    [ ${BRIGHT%.*} -gt $UPPER ] && {
+	LIGHT=100
+    } || {
+	LIGHT=$(( ${BRIGHT%.*}*(100/UPPER) ))
+    }
+}
+LIGHT=${LIGHT%%.*}
+
+DIFF=$(($(xbacklight -get | cut -f1 -d.) - $LIGHT))
+
+[ $DIFF -gt $TOLERANCE -o $DIFF -lt -$TOLERANCE ] && {
+    echo Setting backlight to $LIGHT
+    xbacklight -set ${LIGHT%%.*}
+}
+
+rm "$IMG"
