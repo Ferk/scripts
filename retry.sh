b9ccff8dd24bd02b28f3e88458ef3f39a346efc3
Fernando C.V <ferkiwi@gmail.com>
2012-06-26 11:39:00 +0200

    retry.sh - it will now accept the arg quoted to include && and || in the retry command. Also, added some color

--- retry.sh
+++ retry.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-$@
+eval $@
 while [ "$?" != "0" ]
 do
-    echo "err=\"$?\" - Retrying..."
+    echo -e "\e[31m${0##*/}\e[0m: evaluation exit value $? - Retrying..."
     sleep 1
-    $@
+    eval $@
 done
