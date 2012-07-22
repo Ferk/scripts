e6326222a3d8a3c9645488698e079c354584cb80
Fernando C.V <ferkiwi@gmail.com>
2012-07-22 15:57:24 +0200

    Some comments added

--- idlescript.sh
+++ idlescript.sh
@@ -4,6 +4,14 @@
 # inactivity from user and system, and sets the computer to suspend
 # status.
 #
+# I wrote this script because most other suspend-on-idle methods don't
+# take HD activity into account, which is required when you let the
+# computer alone working on some operation, and want it to
+# automatically suspend when the operation is finished.
+#
+# If you want the computer to wake up automatically at some given time
+# you can use the command: rtcwake -t <time>
+#
 # Requires pm-utils
 #
 # by Fernando Carmona Varo
@@ -24,6 +32,7 @@ echo "Limit: $IOLIMIT"
 
 # weighted milliseconds the HD spent doing I/Os
 HD0=$(cat /sys/block/sda/stat | awk '{print $11}')
+# keyboard interruptions
 KB0=$(cat /proc/interrupts | awk '/i8042/ {print $2; exit}')
 while true
 do
@@ -32,8 +41,7 @@ do
     HD1=$(cat /sys/block/sda/stat | awk '{print $11}')
     KB1=$(cat /proc/interrupts | awk '/i8042/ {i += $2+$3}; END { print i }')
     
-    echo "[$(date +%F_%H%M)] HD Usage: $(($HD1-$HD0))"
-    echo "Keyboard activity: $((KB1-KB0))"
+    echo "[$(date +%F_%H%M)] Activity:  HD:$(($HD1-$HD0)) Keyboard:$((KB1-KB0))"
 
     # Actions to activate when the Hard Disk is inactive and no user input
     [ $(($HD1-$HD0)) -lt $IOLIMIT ]  && [ $((KB1-KB0)) = 0 ] && { 
