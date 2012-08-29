9a23eeacca51ab49cf1708267d7830ea559aead0
Fernando C.V <ferkiwi@gmail.com>
2012-08-29 13:38:17 +0200

    convert.sh: will now list files with errors at the end instead of aborting on error. Files already existing will be skipped

--- convert.sh
+++ convert.sh
@@ -22,24 +22,34 @@ while [ "$1" ]; do
 
     [ -f "$fname" ] || { echo "file doesn't exist: $fname"; exit 1;}
 
-    fext=${fname##*.}
+    # get file extension in lowercase
+    fext=$( echo ${fname##*.} | tr '[A-Z]' '[a-z]' )
 
     echo "converting $fname"
     case $fext in
         avi|mp4|wmv|mkv)
-            ffmpeg -i "$fname" $bitrate "${fname%.*}.webm"
+	    dest="${fname%.*}.webm"
+	    [ -e dest ] && { echo "destination file already exists: $dest"; exit 1;}
+            ffmpeg -i "$fname" $bitrate "$dest"
             ;;
         png|jpg)
-	    [ -e "${fname%.*}.webp" ] || \
-            cwebp -quiet -m 6 "$fname" -o "${fname%.*}.webp"
+	    dest="${fname%.*}.webp"
+	    [ -e dest ] && { echo "destination file already exists: $dest"; exit 1;}
+            cwebp -quiet -m 6 "$fname" -o "$dest"
             ;;
 	ogg)
 	    ;;
         *)
             echo "no conversion available for format \"${fext}\": $fname"
     esac || {
-        echo "Error in conversion of ${fname}"
-        exit 2
+	errors="$errors ${fname}\n"
     }
 
 done
+
+if [ "$errors" ]
+then
+    echo "Errors were found in the following files and they were not converted:"
+    echo -e "$errors"
+fi
+
