204ff8a597e4b6422fa3da30b70e1b50c9d6a2a4
Fernando C.V <ferkiwi@gmail.com>
2013-02-01 00:58:51 +0100

    Now dwm will be recompiled if the config.h was edited, also some refactoring was made

--- dwm.sh
+++ dwm.sh
@@ -7,56 +7,82 @@
 ##
 
 
-    # Update the status info in the bar
-    statusupd() {
-        TIME=$(date '+%a %F --%H:%M--')
-        LOAD=$(uptime | sed 's/.*, //')
+BUILDIR="$XDG_CONFIG_HOME/dwm/"
 
-        #BAT=$(acpi -b | cut -d, -f 2)
-        STAT=$(statck -1)
-        #TRAFIC=$(vnstat --oneline | cut -d ";" -f 11)
+[ "$DISPLAY" ] || { echo "error: no DISPLAY available"; exit 1; }
 
-	# check if my neightbor is connected
-	HOSTS=""
-	for h in 1
-	do
-	    ping -c 1 "192.168.1.$h" && HOSTS="${HOSTS}.$h"
-	done
-	# use reduced traffic speed if it's connected
-	if [ $(echo $HOSTS | wc -c) -gt 3 ]; then
-	    transmission-remote --alt-speed
-	else
-	    transmission-remote --no-alt-speed
-	fi
-
-        #xsetroot -name "$TIME {$STAT} <$LOAD> -$TRAFIC-"
-        xsetroot -name "^[f3;$TIME ^[f2;^[i2;^[f0;$STAT^[i29; ^[f77F;$LOAD^[i15; ^[f292;^[i35;$HOSTS "
-    }
-    [ "$1" = "update" ] && {
-	statusupd
-	exit
-    }
+# Update the status info in the bar
+statusupd() {
+    TIME=$(date '+%a %F --%H:%M--')
+    LOAD=$(uptime | sed 's/.*, //')
     
-    while true
-    do
-	statusupd
-	sleep 40
-    done &
+    #BAT=$(acpi -b | cut -d, -f 2)
+    STAT=$(statck -1)
+    #TRAFIC=$(vnstat --oneline | cut -d ";" -f 11)
 
-    # if dwm is already running, only spawn status updater
-    pgrep "dwm$" >/dev/null && {
-	echo "dwm is already running, a status update daemon has been spawned, but dwm won't be re-run"
-	exit
-    }
+    # # check if my neightbor is connected
+    # HOSTS=""
+    # for h in 1
+    # do
+    # 	ping -c 1 "192.168.1.$h" && HOSTS="${HOSTS}.$h"
+    # done
+ 
+    # # use reduced traffic speed if it's connected
+    # if [ $(echo $HOSTS | wc -c) -gt 3 ]
+    # then
+    # 	transmission-remote --alt-speed
+    # else
+    # 	transmission-remote --no-alt-speed
+    # fi
 
-    # Set a known name to the WM so that some programs don't complain
-    if hash wname; then wmname LG3D
-    else xprop -root -f _NET_WM_NAME 8s -set _NET_WM_NAME "LG3D"
-    fi
+    # number of ARP entries
+    HOSTS=$(arp -a | wc -l)
+    
+    xsetroot -name "$TIME {$STAT} <$LOAD> ☯$HOSTS"
+        #xsetroot -name "^[f3;$TIME ^[f2;^[i2;^[f0;$STAT^[i29; ^[f77F;$LOAD^[i15; ^[f292;^[i35;$HOSTS "
+}
 
+[ "$1" = "update" ] && {
+    statusupd
+    exit
+}
+
+while true
+do
+    statusupd
+    sleep 40
+done &
+
+# if dwm is already running, only spawn status updater
+pgrep "dwm$" >/dev/null && {
+    echo "dwm is already running, a status update daemon has been spawned, but dwm won't be re-run"
+    exit
+}
+
+# Set a known name to the WM so that some programs don't complain
+if hash wmname &>/dev/null
+then 
+    wmname LG3D
+else 
+    xprop -root -f _NET_WM_NAME 8s -set _NET_WM_NAME "LG3D"
+fi
 
 # Run dwm in loop so it can restart (to close session pkill dwm.sh)
-while [ 1 ]
+while true
 do
-    ~/bin/dwm/dwm
+    # Compile dwm if newer config is available
+    if [ "$BUILDIR/config.h" -nt "$BUILDIR/dwm"  ]
+    then
+	( cd "$BUILDIR" && make )
+    fi
+
+    # Run custom dwm if it exists (even if compilation failed)    
+    if [ -e "$BUILDIR/dwm"  ]
+    then
+	"$BUILDIR/dwm"
+    else 
+	dwm
+    fi
 done
+
+
